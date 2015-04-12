44eb291dd76af04ce24b9708bfb5fa5eb9c70d59
Roberto Palamaro <roby.palamaro@gmail.com>
2015-04-12 14:52:26 +0200

    Initial Commit

---
+++ qLearning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  qLearning
+//
+//  Created by Roberto Palamaro on 12/04/15.
+//  Copyright (c) 2015 Roberto Palamaro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
